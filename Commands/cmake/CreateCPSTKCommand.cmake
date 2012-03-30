b90659d668693499c10a867092252bd5b0e77bde
Nicolas Toussaint <nicolas.toussaint@kcl.ac.uk>
2012-03-30 13:47:15 +0200

    now the toolkit is named CPSTK, for cardiac prolate spheroidal toolkit

--- Commands/cmake/CreateCPSTKCommand.cmake
+++ Commands/cmake/CreateCPSTKCommand.cmake
@@ -1,17 +1,17 @@
-function(CreatePTKCommand command_name keyword description src_dir)
+function(CreateCPSTKCommand command_name keyword description src_dir)
 
   set(FILE_NAME  "itk${command_name}CommandFactory")
   set(DESCRIPTION ${description})
   string(TOUPPER ${FILE_NAME} HEADER_PROT)
 
-  configure_file(${src_dir}/itkPtkCommandFactoryTemplate.h.in
+  configure_file(${src_dir}/itkCpstkCommandFactoryTemplate.h.in
     ${CMAKE_CURRENT_BINARY_DIR}/${FILE_NAME}.h @ONLY)
-  configure_file(${src_dir}/itkPtkCommandFactoryTemplate.cxx.in
+  configure_file(${src_dir}/itkCpstkCommandFactoryTemplate.cxx.in
     ${CMAKE_CURRENT_BINARY_DIR}/${FILE_NAME}.cxx @ONLY)
   
   set(FILE_NAME  "itk${command_name}Command")
   string(TOUPPER ${FILE_NAME} HEADER_PROT)
-  configure_file(${src_dir}/itkPtkCommandTemplate.h.in
+  configure_file(${src_dir}/itkCpstkCommandTemplate.h.in
     ${CMAKE_CURRENT_BINARY_DIR}/${FILE_NAME}.h @ONLY)
   
 endfunction()
@@ -20,4 +20,4 @@ string(REPLACE " " ";" COMMAND_NAME ${MY_COMMAND_NAME})
 string(REPLACE " " ";" DESCRIPTION  ${MY_DESCRIPTION})
 string(REPLACE " " ";" KEYWORD  ${MY_KEYWORD})
 
-CreatePTKCommand(${MY_COMMAND_NAME} ${MY_KEYWORD} ${MY_DESCRIPTION} ${SRC_DIR})
+CreateCPSTKCommand(${MY_COMMAND_NAME} ${MY_KEYWORD} ${MY_DESCRIPTION} ${SRC_DIR})
