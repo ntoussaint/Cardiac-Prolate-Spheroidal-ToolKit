032eb80b9fa0f729c0aa718c08ee061cf38d0df2
Nicolas Toussaint <nicolas.toussaint@kcl.ac.uk>
2012-04-15 14:19:47 +0100

    adding the gradient factor as a parameter for the structure estimation, and allow writing results in a vtk file

--- Commands/itkTensorMeshStructureCommand.cxx
+++ Commands/itkTensorMeshStructureCommand.cxx
@@ -42,12 +42,13 @@ namespace itk
     m_LongDescription += m_ShortDescription;
     m_LongDescription += "\n\nUsage:\n";
     
-    m_LongDescription += "-i    [input  tensor image]\n";
+    m_LongDescription += "-i    [input  tensor image/mesh]\n";
+    m_LongDescription += "-d    [domain file]\n";
     m_LongDescription += "-pr   [prolate transform used]\n";
     m_LongDescription += "-f1   [forward displacement field (default : forward.mha)]\n";
     m_LongDescription += "-f2   [backward displacement field (default : backward.mha)]\n";  
     m_LongDescription += "-o    [output structure tensor field]\n";
-    
+    m_LongDescription += "-f    [gradient evaluation factor (default=1.0)]\n";
   }
 
   TensorMeshStructureCommand::~TensorMeshStructureCommand()
@@ -91,37 +92,53 @@ namespace itk
     }
     const char* inputfile                    = cl.follow("input.vtk",2,"-i","-I");
     const char* prolatefile                  = cl.follow("prolate.pr",2,"-pr","-PR");
+    const char* domainfile                   = cl.follow("nofile",2,"-d","-D");
     const char* displacementfieldfile        = cl.follow("forward.mha",2,"-f1","-F1");
     const char* inversedisplacementfieldfile = cl.follow("backward.mha",2,"-f2","-F2");
     const char* outputfile                   = cl.follow("output.mha",2,"-o","-O");
+    const double factor                   = cl.follow(1.0,2,"-f","-F");
   
     std::cout << "inputfile: \t\t\t" << inputfile << std::endl;
+    std::cout << "factor: \t\t\t" << factor << std::endl;
     std::cout << "prolatefile: " << prolatefile << std::endl;
+    std::cout << "domainfile: " << domainfile << std::endl;
     std::cout << "displacementfieldfile: " << displacementfieldfile << std::endl;
     std::cout << "inversedisplacementfieldfile: " << inversedisplacementfieldfile << std::endl;
     std::cout << "outputfile: \t\t\t" <<outputfile << std::endl;
   
     std::cout << std::flush;
   
-    std::cout << "Reading input tensor image: " << inputfile <<"... "<< std::flush;
-    TensorIOType::Pointer reader = TensorIOType::New();
-    ImageReaderType::Pointer t_reader = ImageReaderType::New();
-    
-    reader->SetFileName(inputfile);
-    t_reader->SetFileName(inputfile);
-  
-    try
+    std::cout << "Reading input tensor image/mesh: " << inputfile <<"... "<< std::flush;
+    std::string extension = itksys::SystemTools::GetFilenameLastExtension(inputfile).c_str();
+
+    MeshType::Pointer data = NULL;
+    if (strcmp (extension.c_str(), ".vtk") == 0)
     {
+      TensorMeshIOType::Pointer reader = TensorMeshIOType::New();
+      reader->SetFileName (inputfile);
       reader->Read();
-      t_reader->Update();
+      data = reader->GetOutput();
     }
-    catch(itk::ExceptionObject &e)
+    else
     {
-      std::cerr << e << std::endl;
-      std::exit(EXIT_FAILURE);
+      TensorIOType::Pointer reader = TensorIOType::New();
+      reader->SetFileName(inputfile);    
+      reader->Read();
+
+      ImageToMeshFilterType::Pointer imagetomesh = ImageToMeshFilterType::New();
+      imagetomesh->SetInput (reader->GetOutput());
+      imagetomesh->Update();
+      data = imagetomesh->GetOutput();
     }
+    std::cout<<" Done."<<std::endl;
+
+    std::cout << "Reading domain : " << domainfile <<"... "<< std::flush;
+    ImageReaderType::Pointer reader = ImageReaderType::New();
+    reader->SetFileName(domainfile);
+    reader->Update();
+    ImageType::Pointer domain = reader->GetOutput();
     std::cout << " Done." << std::endl;
-  
+    
     std::cout << "Reading forward field: " << displacementfieldfile <<"... "<<std::flush;
     DisplacementFileReaderType::Pointer displacementreader = DisplacementFileReaderType::New();
     displacementreader->SetFileName(displacementfieldfile);
@@ -162,43 +179,69 @@ namespace itk
 
     TransformType::Pointer backtransform = TransformType::New();
     transform->GetInverse (backtransform);
-    
-    ImageToMeshFilterType::Pointer image2mesh = ImageToMeshFilterType::New();
-    image2mesh->SetInput (reader->GetOutput());
-    image2mesh->Update();
 
+    data->DisconnectPipeline();
+    
+    std::cout<<"switching coordinates... "<<std::flush;
     WarperType::Pointer warper = WarperType::New();
     warper->SetDisplacementField (displacementfield);
     warper->SetInverseDisplacementField (inversedisplacementfield);
-    warper->SetInput (image2mesh->GetOutput());
+    warper->SetInput (data);
     warper->Update();
 
     TransformerType::Pointer transformer = TransformerType::New();
     transformer->SetTransform (transform);
     transformer->SetInput (warper->GetOutput());
     transformer->Update();
+    std::cout<<"done."<<std::endl;
     
-    MeshType::Pointer data;
+    MeshType::Pointer dataforstructure;
 
     if (use_prolate)
-      data = transformer->GetOutput();
+      dataforstructure = transformer->GetOutput();
     else
-      data = image2mesh->GetOutput();
+      dataforstructure = data;
     
-    data->DisconnectPipeline();
+    dataforstructure->DisconnectPipeline();
 
+    double position_max[3] = {-100,-100,-100};
+    double position_min[3] = {+100,+100,+100};
+
+    for (unsigned int i=0; i < dataforstructure->GetNumberOfPoints(); i++)
+    {
+      MeshType::PointType p;
+      dataforstructure->GetPoint (i, &p);
+      double h[3];
+      transform->EvaluateScaleFactors (p.GetDataPointer(), h);
+      
+      for (unsigned int j=0; j<3; j++)
+      {
+	position_max[j] = std::max (h[j], position_max[j]);
+	position_min[j] = std::min (h[j], position_min[j]);
+      }
+    }
+
+    std::cout<<"bounds : "<<std::endl;
+    std::cout << position_min[0] << " : " << position_max[0] <<std::endl;
+    std::cout << position_min[1] << " : " << position_max[1] <<std::endl;
+    std::cout << position_min[2] << " : " << position_max[2] <<std::endl;
     
+    
+    std::cout<<"evaluating structure... "<<std::flush;
     FilterType::Pointer structurefilter = FilterType::New();
-    structurefilter->SetInput (0, data);
-    structurefilter->SetInput (1, data);
+    structurefilter->SetInput (0, dataforstructure);
+    structurefilter->SetInput (1, dataforstructure);
     structurefilter->SetTransform (transform);
     structurefilter->SetUsePiWorkAround(use_prolate);
+    structurefilter->SetGradientUFactor (factor);
     
     structurefilter->Update();
+    std::cout<<"done."<<std::endl;
 
     MeshType::Pointer structure = structurefilter->GetOutput();
     structure->DisconnectPipeline();
-    
+
+    std::cout<<"switching coordinates... "<<std::flush;
     TransformerType::Pointer backtransformer = TransformerType::New();
 
     if (use_prolate)
@@ -224,16 +267,30 @@ namespace itk
       output = structure;
     
     output->DisconnectPipeline();
+    std::cout<<"done."<<std::endl;
 
+    std::cout<<"transforming to image for AHA separation... "<<std::flush;
     MeshToImageFilterType::Pointer mesh2image = MeshToImageFilterType::New();
     mesh2image->SetInput (output);
-    mesh2image->SetDomain (t_reader->GetOutput());
+    mesh2image->SetDomain (domain);
     mesh2image->Update();
-    
-    TensorIOType::Pointer outputwriter = TensorIOType::New();
-    outputwriter->SetInput (mesh2image->GetOutput());
-    outputwriter->SetFileName (outputfile);  
-    outputwriter->Write();
+
+    std::string outextension = itksys::SystemTools::GetFilenameLastExtension(outputfile).c_str();
+
+    if (strcmp (outextension.c_str(), ".vtk") == 0)
+    {
+      TensorMeshIOType::Pointer writer = TensorMeshIOType::New();
+      writer->SetInput (output);
+      writer->SetFileName (outputfile);
+      writer->Write();
+    }
+    else
+    {  
+      TensorIOType::Pointer outputwriter = TensorIOType::New();
+      outputwriter->SetInput (mesh2image->GetOutput());
+      outputwriter->SetFileName (outputfile);
+      outputwriter->Write();
+    }
     
     AHALimiterType::Pointer zonelimiter = AHALimiterType::New();
     zonelimiter->SetInput (mesh2image->GetOutput());
@@ -242,7 +299,9 @@ namespace itk
     zonelimiter->SetInverseDisplacementField (inversedisplacementfield);
     zonelimiter->CanineDivisionsOff();
     zonelimiter->SetAHASegmentationType (AHALimiterType::AHA_17_ZONES);
-    
+    std::cout<<"done."<<std::endl;
+
+    std::cout<<"computing structure covariance within zones... "<<std::flush;
     MeshType::Pointer               zonestructure = MeshType::New();
     MeshType::PointsContainer::Pointer points     = MeshType::PointsContainer::New();
     MeshType::PointDataContainer::Pointer tensors = MeshType::PointDataContainer::New();
@@ -253,6 +312,7 @@ namespace itk
     
     for (unsigned int i=1; i<=zonelimiter->GetNumberOfAHAZones(); i++)
     {
+      std::cout<<"======= zone "<<i<<" ======= "<<std::flush;
       zonelimiter->SetAHAZone (i);
       ImageToMeshFilterType::Pointer zoneimage2mesh = ImageToMeshFilterType::New();
       zoneimage2mesh->SetInput (zonelimiter->GetOutput());
@@ -261,9 +321,10 @@ namespace itk
 
       MeshType::PointType p = zonelimiter->GetZoneCentralPointCartesian();
       TensorType t (0.0);
-
-      double ratio = 1.3;
       
+      double ratio = 1.3;
+
+      std::cout<<"( N = "<<zone->GetNumberOfPoints()<<") "<<std::flush;
       for (unsigned int j=0; j<zone->GetNumberOfPoints(); j++)
       {
 	TensorType l (0.0); zone->GetPointData (j, &l);
@@ -289,6 +350,7 @@ namespace itk
 
       zonestructure->SetPoint (i-1, p);
       zonestructure->SetPointData (i-1, t);
+      std::cout<<"done."<<std::endl;
     }
 
     std::cout<<"Writing zone structure tensors to "<<"zones-structure.vtk"<<"... "<<std::flush;
